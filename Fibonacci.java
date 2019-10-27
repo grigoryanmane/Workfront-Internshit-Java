198cfd031835662acefad7d0b14242ee9387e511
grigoryanmane <56272266+grigoryanmane@users.noreply.github.com>
2019-10-28 00:59:37 +0400

    Fibonacci Problem using recursion and for loos

---
+++ Fibonacci.java
@@ -0,0 +1,26 @@
+package homework1;
+
+public class Fibonacci {
+    public static int fib(int number) {
+        if (number == 0) {
+            return 0;
+        } else if (number == 1) {
+            return 1;
+        } else {
+            return fib(number - 1) + fib(number - 2);
+        }
+    }
+
+    public static void printFibonacci(int number) {
+        for (int i = 0; i <= number; i++) {
+            System.out.println(fib(i));
+        }
+    }
+
+    public static void main(String[] args) {
+        //The problem could have been solved in a better way by using arrays and storing
+        //the fib values in them for the later use, however, it was mentioned in the instructions
+        //to use recursion for the problem
+        printFibonacci(7);
+    }
+}
